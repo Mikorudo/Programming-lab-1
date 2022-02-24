84a61eebfdf7106356884cfbf9dc10d9b4830b85
Михаил Косточкин <kostochkin2002@inbox.ru>
2022-02-24 14:29:37 +0700

    0

--- Programming lab 1/LargeArrayCreation.cpp
+++ Programming lab 1/LargeArrayCreation.cpp
@@ -1,9 +1,14 @@
 #include "LargeArrayCreation.h"
+#include "PageStruct.h"
 #include <string>
 #include <fstream>
 #include <exception>
+#include <bitset>
 
 LargeArrayCreation::LargeArrayCreation(long _size, std::string _filename = "default.dat") {
+	if (size < 0) {
+		throw std::exception("Negative array size");
+	}
 	filename = _filename;
 	size = _size;
 	filePtr = new std::fstream(filename, std::ios::binary | std::ios::in | std::ios::out);
@@ -14,4 +19,7 @@ LargeArrayCreation::LargeArrayCreation(long _size, std::string _filename = "defa
 LargeArrayCreation::~LargeArrayCreation() {
 	if (filePtr != NULL)
 		delete filePtr;
+}
+long LargeArrayCreation::GetArraySize() const {
+	return size;
 }
--- Programming lab 1/LargeArrayCreation.h
+++ Programming lab 1/LargeArrayCreation.h
@@ -1,19 +1,24 @@
 #pragma once
+
 class LargeArrayCreation {
 public:
+	const int intSize = sizeof(int);
+	const int pageSize = 512 * 1024;
+	const int intCount = pageSize / intSize;
 	LargeArrayCreation(long _size, std::string _filename);
-	int /*сменить тип данных*/ GetAddress(long index);
-	int GetValue(long index, int& result);
+	void* /*сменить тип данных*/ GetAddress(long index) const;
+	int GetValue(long index, int& result) const;
 	int SetValue(long index, int value);
+	long GetArraySize() const;
 	/*Переопределение []*/
 	~LargeArrayCreation();
 private:
 	std::string filename;
 	long size;
 	std::fstream* filePtr;
-	/*Буфер страниц*/
-	/*Массив битовых карт страницы*/
-	/**/
+	Page pages[5];
+	std::bitset<intCount> bits; 
 };
 
 /* std::ios::app - возможная ошибка*/
+//Модификация страницы
